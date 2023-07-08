0e2458be3cccee0a06875506cc4e19ec3886f6b6
Alex Rud <lexa.roach@gmail.com>
2023-07-08 23:41:06 +0300

    init

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,14 +3,9 @@ import { data } from "../../utils/data";
 
 function App() {
   return (
-    <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
-      </pre>
-    </div>
+    <>
+    
+    </>
   );
 }
 
